fa6a69b9e2537cba15e599cd2a028d874b18ca01
Shrutika Gamare <shrutikagamare@Shrutikas-MacBook-Pro.local>
2018-07-11 15:10:11 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express=require('express');
+var app=express();
+
+// path / is my root route!
+app.get('/',function(req,res) {
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+var PORT=process.env.PORT || 8080;
+
+var server=app.listen(PORT, function(req, res)
+{
+    console.log("server started!");
+    
+})
